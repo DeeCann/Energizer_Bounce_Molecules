0d1f209329a8ef09cbc69738b9ada0f5f2728f35
Bartlomiej Przybylak <bartlomiej.przybylak@gmail.com>
2015-07-07 21:17:22 +0200

    Przed mergem

---
+++ Assets/Scripts/Game/LevelPack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelPack : MonoBehaviour {
+
+	public Transform LevelStandardPanel;
+	public Transform Back;
+
+	public void OpenPanel() {
+		LevelStandardPanel.GetComponent<Animator>().SetBool("FadeIn", false);
+		LevelStandardPanel.GetComponent<Animator>().SetBool("FadeOut", true);
+
+		GetComponent<Animator>().SetBool("FadeOut", false);
+		GetComponent<Animator>().SetBool("FadeIn", true);
+	}
+
+	public void ClosePanel() {
+		GetComponent<Animator>().SetBool("FadeIn", false);
+		GetComponent<Animator>().SetBool("FadeOut", true);
+
+		LevelStandardPanel.GetComponent<Animator>().SetBool("FadeOut", false);
+		LevelStandardPanel.GetComponent<Animator>().SetBool("FadeIn", true);
+	}
+}
