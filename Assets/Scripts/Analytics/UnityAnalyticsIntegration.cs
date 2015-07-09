2795fb6eaec32abedc79634fc131e7959940ec29
Bartlomiej Przybylak <bartlomiej.przybylak@gmail.com>
2015-07-09 10:33:26 +0200

    Restart analyticsa

--- Assets/Scripts/Analytics/UnityAnalyticsIntegration.cs
+++ Assets/Scripts/Analytics/UnityAnalyticsIntegration.cs
@@ -3,11 +3,10 @@ using System.Collections;
 using UnityEngine.Cloud.Analytics;
 
 public class UnityAnalyticsIntegration : MonoBehaviour {
-	
+
 	void Start () {
+		
 		const string projectId = "8e28fe4f-15dc-454f-9ca7-e521b7f1051f";
 		UnityAnalytics.StartSDK (projectId);
-
 	}
-
 }
