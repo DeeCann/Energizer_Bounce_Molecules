c35c7adbba6bd62a2f235bf08a312a3c5ff2f065
Bartlomiej Przybylak <bartlomiej.przybylak@gmail.com>
2015-08-03 22:57:18 +0200

    Glass - LVL1 zawiera pękający element szklany

---
+++ Assets/Scripts/Elements/Glass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glass : MonoBehaviour {
+	[SerializeField]
+	private ParticleSystem particles;
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if(other.collider.tag == Tags.Molecule) {
+			GetComponent<SpriteRenderer>().enabled = false;
+			GetComponent<Shadow>().enabled = false;
+			GetComponent<BoxCollider2D>().enabled = false;
+			Destroy(transform.FindChild("New Game Object").gameObject);
+			particles.Emit(100);
+			Destroy(this, 1);
+		}
+	}
+
+}
